f1ca2fc1ac7b21c138a25861ce5d1b1349613955
Vyshnav <Vyshnavc235@gmail.com>
2023-06-17 22:04:32 +0530

    font fam, icons done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { MachineCardComponent } from './ui-components/machine-card/machine-card.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    MachineCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ui-components/machine-card/machine-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+@Component({
+  selector: 'app-machine-card',
+  templateUrl: './machine-card.component.html',
+  styleUrls: ['./machine-card.component.scss'],
+  
+})
+export class MachineCardComponent {
+
+}
